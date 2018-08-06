0ad50fabe24a441a96d0a6d83a368d43d4289432
Poschi <development@poschi.net>
2018-08-06 22:00:48 +0200

    Voice delay konfigurierbar

--- plugins/voice.py
+++ plugins/voice.py
@@ -1,4 +1,5 @@
 import time
+import ConfigParser
 from plugin import Plugin
 from thread import start_new_thread
 
@@ -6,11 +7,16 @@ class Voice(Plugin):
   def __init__(self, config=None):
     super(Voice, self).__init__()
 
+    try:
+      self.delay=int(config.get('Voice', 'delay'))
+    except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
+      self.delay = 5
+
   def on_join(self, bot, user_nick, host, channel):
     #bot.send_message("#" + channel, "Hallo2 " + user_nick, user_nick)
-    start_new_thread(mode, (bot, channel, user_nick,))
+    start_new_thread(mode, (bot, channel, user_nick, self.delay,))
   
-def mode(bot, channel, user_nick):
+def mode(bot, channel, user_nick, delay):
     #bot.send_message("#" + channel, "Warte fuer " + user_nick, user_nick)
-    time.sleep(5)
+    time.sleep(delay)
     bot.irc.send_raw("MODE #" + channel + " +v " + user_nick)
