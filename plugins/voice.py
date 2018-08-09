d9198fd6b0eca7f4b5c44ef135f322257c1e8763
Florian Baumann <flo@noqqe.de>
2018-08-09 11:07:10 +0200

    delay is now longer

--- plugins/voice.py
+++ plugins/voice.py
@@ -10,12 +10,12 @@ class Voice(Plugin):
     try:
       self.delay=int(config.get('Voice', 'delay'))
     except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
-      self.delay = 5
+      self.delay = 30
 
   def on_join(self, bot, user_nick, host, channel):
     #bot.send_message("#" + channel, "Hallo2 " + user_nick, user_nick)
     start_new_thread(mode, (bot, channel, user_nick, self.delay,))
-  
+
 def mode(bot, channel, user_nick, delay):
     #bot.send_message("#" + channel, "Warte fuer " + user_nick, user_nick)
     time.sleep(delay)
